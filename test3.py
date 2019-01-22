fa98c5f7a19e32968701a1b7bb688394080e2370
brady <acheng_69@163.com>
2019-01-22 17:52:35 +0800

    Python Demo

--- test3.py
+++ test3.py
@@ -5,3 +5,13 @@
 # @File    : test3.py
 # @Software: PyCharm
 
+# name = input('please enter your name: ')
+# print('Hello,', name)
+
+def power(x, n):
+    s = 1
+    while n > 0:
+        n = n - 1
+        s = s * x
+    return s
+print(power(5, 2))
