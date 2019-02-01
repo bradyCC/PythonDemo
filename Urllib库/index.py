f4e68420d746cea867319894829d87a009bb2b91
brady <acheng_69@163.com>
2019-02-01 17:50:21 +0800

    Python Demo

--- Urllib库/index.py
+++ Urllib库/index.py
@@ -6,6 +6,12 @@
 # @Software: PyCharm
 
 # Urllib是Python提供的一个用于操作URL的模块，我们爬取网页的时候，经常需要用到这个库。
+# urllib.request  请求模块
+# urllib.error  异常处理模块
+# urllib.parse  url解析模块
+# urllib.robotparser  robots.txt解析模块
+
+
 import urllib.request
 
 file = urllib.request.urlopen('http://www.baidu.com')
