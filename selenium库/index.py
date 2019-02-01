3de4cc3410e15d26a01e8b3114bbad498b4eebec
brady <acheng_69@163.com>
2019-02-01 16:54:36 +0800

    Python Demo

---
+++ selenium库/index.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/2/1 16:53
+# @Author  : Brady
+# @Email   : acheng_69@163.com
+# @File    : index.py
+# @Software: PyCharm
+
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('http://www.baidu.com')
+
