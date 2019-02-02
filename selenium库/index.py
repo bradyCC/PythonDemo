4465de76e36e138721a83888100c3010130b48c6
brady <acheng_69@163.com>
2019-02-02 10:33:45 +0800

    Python Demo

--- selenium库/index.py
+++ selenium库/index.py
@@ -5,8 +5,24 @@
 # @File    : index.py
 # @Software: PyCharm
 
+# 自动化测试工具，支持多种浏览器。爬虫中主要用来解决JavaScript渲染的问题。
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
-driver = webdriver.Chrome()
-driver.get('http://www.baidu.com')
-
+browser = webdriver.Chrome()
+try:
+    browser.get('https://www.baidu.com')
+    input = browser.find_element_by_id('kw')
+    input.send_keys('Python')
+    input.send_keys(Keys.ENTER)
+    wait = WebDriverWait(browser, 10)
+    wait.until(EC.presence_of_all_elements_located((By.ID, 'content_left')))
+    print(browser.current_url)
+    print(browser.get_cookies())
+    print(browser.page_source)
+finally:
+    browser.close()
