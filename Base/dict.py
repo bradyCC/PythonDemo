39cda9497b7abf8407706a06237b0425c3a10451
brady <acheng_69@163.com>
2019-05-11 18:04:43 +0800

    Python Demo

--- Base/dict.py
+++ Base/dict.py
@@ -5,3 +5,13 @@
 # @File    : dict.py
 # @Software: PyCharm
 
+# Python中判断key是否存在的方法有两种： 1- key in dist  2- d.get(key)
+d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+d['Michael'] = 80                            # 赋值
+print(d['Michael'])
+d.pop('Bob')                                 # 删除
+print(d)
+
+
+
+
