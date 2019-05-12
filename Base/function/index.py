5e5fbba09cb16f64d091cfbebf6ff2f7e397efb9
Brady <acheng_69@163.com>
2019-05-12 21:13:58 +0800

    Python Demo

---
+++ Base/function/index.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019-05-12 09:15
+# @Author  : brady
+# @Email   : acheng_@163.com
+# @File    : function.py
+# @Software: PyCharm
+
+# # 内置函数
+# print(abs(-10))         # 取绝对值
+# print(max(1, 5, 100))   # 取最大值
+# print(min(1, 5, 100))   # 取最小值
+#
+# # 数据类型转换
+# print(int('123'))       # 转换为整数
+# print(float('12.34'))   # 转换为浮点数
+# print(str(123))         # 转换为字符串
+# print(bool(1))          # 转换为布尔值
+#
+# print(hex(255))         # 转换为十六进制整数
+
+
+
