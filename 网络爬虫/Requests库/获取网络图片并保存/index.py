34547e0f6e2f2db7d82619f494968c7f7f5c67d1
brady <acheng_69@163.com>
2019-05-13 15:32:43 +0800

    Python Demo

--- 网络爬虫/Requests库/获取网络图片并保存/index.py
+++ 网络爬虫/Requests库/获取网络图片并保存/index.py
@@ -8,22 +8,25 @@
 import requests
 import os
 
-root = 'E://pics//'
-url = 'http://picm.bbzhi.com/fengjingbizhi/alasijiahenanjizhoufeng/show_fengjingou_239550_m.jpg'
-path = root + url.split('/')[-1]
+def saveImg(root, url, path):
+    try:
+        if not os.path.exists(root):
+            os.mkdir(root)
+        if not os.path.exists(path):
+            r = requests.get(url)
+            r.raise_for_status()
+            with open(path, 'wb') as f:
+                f.write(r.content)
+                f.close()
+                print('文件保存成功')
+        else:
+            print('文件已存在')
+    except IOError:
+        print('爬取失败')
 
-try:
-    if not os.path.exists(root):
-        os.mkdir(root)
-    if not os.path.exists(path):
-        r = requests.get(url)
-        r.raise_for_status()
-        with open(path, 'wb') as f:
-            f.write(r.content)
-            f.close()
-            print('文件保存成功')
-    else:
-        print('文件已存在')
-except IOError:
-    print('爬取失败')
 
+if __name__ == '__main__':
+    root = 'E://pics//'
+    url = 'http://picm.bbzhi.com/fengjingbizhi/alasijiahenanjizhoufeng/show_fengjingou_239550_m.jpg'
+    path = root + url.split('/')[-1]
+    saveImg(root, url, path)
