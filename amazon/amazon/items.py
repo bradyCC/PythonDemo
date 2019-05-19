cf4d54e6e0c89cfb503980eaf28938d3452fb443
Brady <acheng_69@163.com>
2019-05-19 21:19:41 +0800

    Python Demo

--- amazon/amazon/items.py
+++ amazon/amazon/items.py
@@ -11,7 +11,4 @@ import scrapy
 class AmazonItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    title = scrapy.Field()
-    url = scrapy.Field()
-    img = scrapy.Field()
     pass
