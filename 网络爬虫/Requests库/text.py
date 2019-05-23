1dcf53ce167904f495ac02ab1feb809bb794b395
brady <acheng_69@163.com>
2019-05-23 13:45:03 +0800

    Python Demo

--- 网络爬虫/Requests库/text.py
+++ 网络爬虫/Requests库/text.py
@@ -6,18 +6,27 @@
 # @Software: PyCharm
 
 import requests
-
+import re
 
 def getHTMLText(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0(Windows NT 10.0; Win64; x64) AppleWebKit/537.36(KHTML, likeGecko) Chrome/74.0.3729.131 Safari/537.36'
+    }
     try:
-        r = requests.get(url, timeout=30)
+        r = requests.get(url, headers=headers, timeout=30)
         r.raise_for_status()
         r.encoding = r.apparent_encoding
-        return r.text
+        japanTitle = re.findall('color:#666666;"(.*?)</span>', r.text, re.S)
+        for item in japanTitle:
+            print(item)
+        chineseTitle = re.findall('color: #039;"(.*?)</a>', r.text, re.S)
+        for item in chineseTitle:
+            print(item)
+        # return r.text
     except IOError:
         print('爬取异常')
 
 
 if __name__ == '__main__':
-    url = 'http://www.bilibili.com'
+    url = 'http://jp.tingroom.com/yuedu/yd300p'
     print(getHTMLText(url))
