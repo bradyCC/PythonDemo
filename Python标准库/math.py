c6361d86434d99543410745e0ecc2f3f0b576c43
brady <acheng_69@163.com>
2019-05-30 17:16:35 +0800

    Python标准库

---
+++ Python标准库/math.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/30 11:38
+# @Author  : Brady
+# @Email   : acheng_69@163.com
+# @File    : index.py
+# @Software: PyCharm
+
+import math
---
+++ Python标准库/sys.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/30 11:55
+# @Author  : Brady
+# @Email   : acheng_69@163.com
+# @File    : index.py
+# @Software: PyCharm
+
+import sys
+
+# print(sys.__doc__)  # 显示 sys 的基本文档
