8c66fd8a0ea9e3965561618ba30dae97e0be06fa
brady <acheng_69@163.com>
2019-05-30 17:34:01 +0800

    Python标准库

---
+++ Python标准库/datetime/datetime.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/30 17:17
+# @Author  : Brady
+# @Email   : acheng_69@163.com
+# @File    : datetime.py
+# @Software: PyCharm
+
+from datetime import *
+
+# now = datetime.now()            # datetime类：2019-05-30 17:30:33.682538
+# now = date(2019, 5, 1)          # date类：默认需要传year、month、day
+# now = date.today()              # 当日：格式为 2019-05-30
+# now = time(12, 30, 0)           # time类：12:30:00
