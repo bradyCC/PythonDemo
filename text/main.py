4aaaa67f28097ab418f667fc19aae7b4ba4c32f4
brady <acheng_69@163.com>
2019-06-01 09:18:06 +0800

    测试

--- text/main.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2019/5/31 11:26
-# @Author  : Brady
-# @Email   : acheng_69@163.com
-# @File    : main.py
-# @Software: PyCharm
-
-from scrapy import cmdline
-cmdline.execute('scrapy crawl customer'.split())
